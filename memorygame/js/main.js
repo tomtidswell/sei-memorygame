b408102b1515c6d8634a1ac8046bb4004e49f697
Tomika <tomtidswell@gmail.com>
2019-04-30 18:13:40 +0100

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,6 +1,18 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("The user flipped a " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0]===cardsInPlay[1]) {
+    console.log("Its a match!!! Well done.");
+  } else {
+    console.log("Better luck next time :(");
+  }
+}
