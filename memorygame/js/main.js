9330f294cb83a555cd55f9193a4983b814a19970
Tomika <tomtidswell@gmail.com>
2019-05-01 09:48:44 +0100

    Created the object structure

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+  {
+    rank:"Queen",
+    suit:"Hearts",
+    cardImage:"images/queen-of-hearts.png"
+  },
+  {
+    rank:"Queen",
+    suit:"Diamonds",
+    cardImage:"images/queen-of-diamonds.png"
+  },
+  {
+    rank:"King",
+    suit:"Hearts",
+    cardImage:"images/king-of-hearts.png"
+  },
+  {
+    rank:"King",
+    suit:"Diamonds",
+    cardImage:"images/king-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -12,14 +34,17 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-  console.log("The user flipped a " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("The user flipped a " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 
 flipCard(1);
 flipCard(2);
 /*
+
 
 var cardOne = cards[0];
 var cardTwo = cards[2];
