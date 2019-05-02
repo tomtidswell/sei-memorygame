8143711997f505356dcf6a3d22381f672e5ed5f1
Tomika <tomtidswell@gmail.com>
2019-05-02 10:07:16 +0100

    Finished game

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,48 +1,121 @@
 var cards = [
   {
+    id: 0,
     rank:"Queen",
     suit:"Hearts",
-    cardImage:"images/queen-of-hearts.png"
+    cardImage:"img/queen-of-hearts.png",
+    randomSeed: 0
   },
   {
+    id: 1,
     rank:"Queen",
     suit:"Diamonds",
-    cardImage:"images/queen-of-diamonds.png"
+    cardImage:"img/queen-of-diamonds.png"
   },
   {
+    id: 2,
     rank:"King",
     suit:"Hearts",
-    cardImage:"images/king-of-hearts.png"
+    cardImage:"img/king-of-hearts.png"
   },
   {
+    id: 3,
     rank:"King",
     suit:"Diamonds",
-    cardImage:"images/king-of-diamonds.png"
+    cardImage:"img/king-of-diamonds.png"
   }
 ];
 
 var cardsInPlay = [];
+var randomisedCards = [];
+var gameBoard = document.getElementById('game-board');
+var gameMessage = document.getElementById('game-message');
+var gameScore = document.getElementById('game-score');
+var gamesPlayed = 0;
+var gamesWon = 0;
+
+document.getElementById('reset-button').addEventListener("click",resetBoard);
+
+function createBoard(){
+  var randomised = [];
+  //randomise the card order
+  for (var i = 0; i < cards.length; i++) {
+    randomised = Math.random();
+    cards[i].randomSeed = randomised;
+    randomisedCards.push(randomised);
+  }
+  randomisedCards.sort();
+
+
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src","img/back.png");
+
+    //loop through the card stack and find the card.id that is equal to the i'th random card
+    cards.forEach( function(card) {
+      if(card.randomSeed===randomisedCards[i]){
+        cardElement.setAttribute("data-id",card.id);
+      }
+    });
+
+    cardElement.addEventListener("click",flipCard);
+    gameBoard.appendChild(cardElement);
+  }
+}
+
+function resetBoard(){
+  gameBoard.innerHTML = "";
+  gameMessage.innerHTML = "&nbsp;";
+  cardsInPlay = [];
+  randomisedCards = [];
+  gameMessage.classList.remove("show");
+  createBoard();
+}
 
 function checkForMatch(){
   if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0]===cardsInPlay[1]) {
-      console.log("Its a match!!! Well done.");
+    if (cardsInPlay[0].rank===cardsInPlay[1].rank) {
+      gameMessage.textContent = "Its a match!!! Well done.";
+      gameMessage.classList.toggle("show");
+      gamesWon++;
     } else {
-      console.log("Better luck next time :(");
+      gameMessage.textContent = "Better luck next time :(";
+      gameMessage.classList.toggle("show");
     }
+    gamesPlayed++;
+    gameScore.textContent = gamesWon+"/"+gamesPlayed
   }
-} 
+}
 
-function flipCard(cardId){
-  console.log("The user flipped a " + cards[cardId].rank);
-  console.log(cards[cardId].suit);
-  console.log(cards[cardId].cardImage);
-  cardsInPlay.push(cards[cardId].rank);
+function flipCard(){
+  var cardId = this.getAttribute('data-id');
+  if(!verifyFlip(cards[cardId])){
+    console.log("Flip not permitted");
+    return false;
+  }
+  cardsInPlay.push(cards[cardId]);
+  this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 }
 
-flipCard(1);
-flipCard(2);
+function verifyFlip(flippedCard){
+  //check that there arent already two flipped cards
+  var allowFlip = cardsInPlay.length >= 2 ? false : true;
+  if(!allowFlip){return false;}
+
+  //check that that card isnt already flipped
+  cardsInPlay.forEach( function(arrayItem) {
+    if(arrayItem.id===flippedCard.id){
+      allowFlip = false;
+    }
+  });
+  if(!allowFlip){return false;}
+  else {return true;}
+}
+
+
+
+createBoard();
 /*
 
 
